93912d760b2ab24258541fd2e673261d42b0f566
jerco2 <jericogodoy7296@gmail.com>
2021-11-20 11:08:49 +0800

    CRUD for Albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,17 +2,17 @@ class AlbumsController < ApplicationController
     before_action :set_album, only: [:edit, :update, :destroy]
 
     def index
-        @albums = Artist.all
+        @albums = Album.all
     end
 
     def new
-        @albums = Artist.new
+        @albums = Album.new
     end
 
     def create
-        @albums = Artist.new(albums_params)
+        @albums = Album.new(albums_params)
         if @albums.save
-            redirect_to artists_path, notice: 'An albums was successfully created.'
+            redirect_to albums_path, notice: 'An albums was successfully created.'
         else
             flash[:error] = "There are some errors encountered."
             render :new
@@ -24,7 +24,7 @@ class AlbumsController < ApplicationController
 
     def update
         if @albums.update(albums_params)
-            redirect_to artists_path, notice: 'An albums was successfully updated.'
+            redirect_to albums_path, notice: 'An albums was successfully updated.'
         else
             flash[:error] = "There are some errors encountered."
             render :edit
@@ -33,16 +33,16 @@ class AlbumsController < ApplicationController
 
     def destroy
         @albums.destroy
-        redirect_to artists_path
+        redirect_to albums_path
     end
 
     private
     
-    def set_artist
-        @albums = Artist.find(params[:id])
+    def set_album
+        @albums = Album.find(params[:id])
     end
     
-    def artist_params
-        params.require(:albums).permit(:name, :permalink, :bio)
+    def albums_params
+        params.require(:album).permit(:name)
     end
 end
