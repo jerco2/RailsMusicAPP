b586444d0d6b57207eaa7cf46964ea23d752e6c6
jerco2 <jericogodoy7296@gmail.com>
2021-11-20 11:45:40 +0800

    CRUD for Tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,48 @@
 class TracksController < ApplicationController
+    before_action :set_track, only: [:edit, :update, :destroy]
+
+    def index
+        @tracks = Track.all
+    end
+
+    def new
+        @tracks = Track.new
+    end
+
+    def create
+        @tracks = Track.new(tracks_params)
+        if @tracks.save
+            redirect_to tracks_path, notice: 'An tracks was successfully created.'
+        else
+            flash[:error] = "There are some errors encountered."
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @tracks.update(tracks_params)
+            redirect_to tracks_path, notice: 'An tracks was successfully updated.'
+        else
+            flash[:error] = "There are some errors encountered."
+            render :edit
+        end
+    end
+
+    def destroy
+        @tracks.destroy
+        redirect_to tracks_path
+    end
+
+    private
+    
+    def set_track
+        @tracks = Track.find(params[:id])
+    end
+    
+    def tracks_params
+        params.require(:track).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   resources :artists
   resources :albums
+  resources :tracks
 end
